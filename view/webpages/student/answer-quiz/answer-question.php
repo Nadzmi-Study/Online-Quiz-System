e09e610a8502bd46dff1bf3954a6ff22e22c08e1
alifzulkafli <alifzulkafli94@gmail.com>
2016-11-17 09:57:32 +0800

    Modified template for answering question

---
+++ view/webpages/student/answer-quiz/answer-question.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created using PhpStorm.
+ * Project: OnlineQuizSystem
+ * Author: seladanghijau
+ * Date: 8/11/2016
+ * Time: 12:21 AM
+ */ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+</head>
+<body>
+<form action="" method="post">
+    <h5>Time: </h5>
+    <div>
+        <h4>Question Desc</h4>
+        <ol>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+        </ol>
+    </div>
+    <div>
+        <h4>Question Desc</h4>
+        <ol>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+        </ol>
+    </div>
+    <input type="submit" name="submit-question" value="Submit Answer"/>
+</form>
+</body>
+</html>
--- view/webpages/student/answer-quiz/index.php
+++ view/webpages/student/answer-quiz/index.php
@@ -6,3 +6,21 @@
  * Date: 8/11/2016
  * Time: 12:21 AM
  */ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+</head>
+<body>
+<form action="" method="post">
+    <div>
+        <h4>Title</h4>
+        <h5>Subject</h5>
+        <h6>Time</h6>
+    </div>
+</form>
+</body>
+</html>
