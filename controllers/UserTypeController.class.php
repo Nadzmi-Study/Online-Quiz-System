43b21f1a19bde15dbd5606a174b08dfcd6a8cbf9
alifzulkafli <alifzulkafli94@gmail.com>
2016-11-19 14:27:12 +0800

    edit UserTypeController getAll()

--- controllers/UserTypeController.class.php
+++ controllers/UserTypeController.class.php
@@ -1,5 +1,15 @@
 <?php
 class UserTypeController
 {
-    public function getUserType($conn){}
+    public function getUserType($conn){
+        $query = "CALL SP_LookupUserType_GetAll()";
+        if($row = mysqli_fetch_array($conn,$query))
+        {
+            $userTypeNo = $row['userTypeNo'];
+            $userTypeDesc = $row['userTypeDesc'];
+
+            $userType = new UserType($userTypeNo, $userTypeDesc);
+        }
+        return $userType;
+    }
 }
