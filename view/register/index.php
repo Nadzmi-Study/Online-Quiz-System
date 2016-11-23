638496b74f609612885bc75fbba6518209f05fee
alifzulkafli <alifzulkafli94@gmail.com>
2016-11-23 23:14:47 +0800

    delete testAlif

--- view/register/index.php
+++ view/register/index.php
@@ -59,7 +59,7 @@ if(isset($_POST["register"])) {
 </html>
 
 <?php
-// funstion declarations:
+// function declarations:
 // displayUserType as drop-down list
 function displayUserType($conn, $userTypeManager) { // display drop-down menu for user type
     $tempUserType = $userTypeManager->getUserType($conn); // get list of user types
---
+++ view/register/register.php
@@ -0,0 +1,25 @@
+<?php
+include_once "../../includes/global.inc.php";
+
+if(isset($_POST["register"])) {
+    $userType = $_POST["userType"];
+    $name = $_POST["name"];
+    $ic = $_POST["ic"];
+    $contact = $_POST["contact"];
+    $email = $_POST["email"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $rePassword = $_POST["re-password"];
+
+    if(($password == $rePassword) || ($userType == 0)) {
+        $newUser = new User($userType, $name, $ic, $contact, $email, $username, $password);
+
+        // call usermanager to register user here
+
+
+        header("Location: ../login");
+    } else
+        header("Location: ../register");
+} else
+    header("Location: ../register");
+?>
