86f64febae9548f23a3b6fa77a7a42dca199a709
alifzulkafli <alifzulkafli94@gmail.com>
2016-11-27 01:08:39 +0800

    Working on create-quiz folder in Lecturer

--- views/lecturer/create-quiz/create-question.php
+++ views/lecturer/create-quiz/create-question.php
@@ -1,3 +1,4 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/Online-Quiz-System/views/includes/global.inc.php";
+//require_once $_SERVER["DOCUMENT_ROOT"] . "/Online-Quiz-System/views/includes/global.inc.php";
+echo "execute SP Insert for quiz, question, and answer";
 ?>
--- views/lecturer/create-quiz/create-quiz.php
+++ views/lecturer/create-quiz/create-quiz.php
@@ -1,3 +1,4 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/Online-Quiz-System/views/includes/global.inc.php";
+//require_once $_SERVER["DOCUMENT_ROOT"] . "/Online-Quiz-System/views/includes/global.inc.php";
+header("Location: ../create-quiz/create-question.html");
 ?>
